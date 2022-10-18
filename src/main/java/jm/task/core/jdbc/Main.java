4b8d608fcb3ad0e339ab7c01feda29eaa4bbf956
ivankasatkin91 <ivankasatkin91@gmail.com>
2022-10-18 21:56:36 +0300

    release 1.02: DAO transaction adjustments

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,7 @@
 package jm.task.core.jdbc;
+
 import jm.task.core.jdbc.model.User;
+
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import java.util.ArrayList;
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,10 +2,11 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-import java.sql.ResultSet;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,23 +27,15 @@ public class UserDaoJDBCImpl implements UserDao {
                 "ENGINE = InnoDB\n" +
                 "DEFAULT CHARACTER SET = utf8;";
 
-        try (Connection con = Util.getInstance().getConnection()) {
-            // turn off auto committing
+        try (Connection con = Util.getInstance().getConnection();
+             PreparedStatement ps = con.prepareStatement(createTable)) {
+
             con.setAutoCommit(false);
-            try (PreparedStatement ps = con.prepareStatement(createTable)) {
-                // create empty table according to prepared statement
-                ps.execute();
-            } catch (SQLException ex) {
-                // terminate transaction and roll back
-                con.rollback();
-                con.setAutoCommit(true);
-                System.out.println("Fail to create a new table!");
-            }
-            // save changes in Data Base
+            ps.execute();
             con.commit();
             con.setAutoCommit(true);
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Fail to create users table!");
         }
     }
 
@@ -50,133 +43,92 @@ public class UserDaoJDBCImpl implements UserDao {
 
         String dropTable = "DROP TABLE IF EXISTS `users`";
 
-        try (Connection con = Util.getInstance().getConnection()) {
-            // turn off auto committing
+        try (Connection con = Util.getInstance().getConnection();
+             PreparedStatement ps = con.prepareStatement(dropTable)) {
+
             con.setAutoCommit(false);
-            // delete table from Data Base
-            try (PreparedStatement ps = con.prepareStatement(dropTable)) {
-                ps.execute();
-            } catch (SQLException ex) {
-                // terminate transaction and roll back
-                con.rollback();
-                con.setAutoCommit(true);
-                System.out.println("Fail to delete a table!");
-            }
-            // save changes in Data Base
+            ps.execute();
             con.commit();
             con.setAutoCommit(true);
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Fail to drop users table!");
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
 
-        String saveUser = "INSERT INTO USERS (name, lastName, age) VALUES (?, ?, ?)";
+        String saveUser = "INSERT INTO `users` (name, lastName, age) VALUES (?, ?, ?)";
 
-        try (Connection con = Util.getInstance().getConnection()) {
-            // turn off auto committing
+        try (Connection con = Util.getInstance().getConnection();
+             PreparedStatement ps = con.prepareStatement(saveUser)) {
+
             con.setAutoCommit(false);
-            // add a new user to the table
-            try (PreparedStatement ps = con.prepareStatement(saveUser)) {
-                ps.setString(1, name);
-                ps.setString(2, lastName);
-                ps.setInt(3, age);
-                ps.executeUpdate();
-            } catch (SQLException ex) {
-                // terminate transaction and roll back
-                con.rollback();
-                con.setAutoCommit(true);
-                System.out.println("Fail to add user to the table!");
-            }
-            // save changes in Data Base
+            ps.setString(1, name);
+            ps.setString(2, lastName);
+            ps.setByte(3, age);
+            ps.executeUpdate();
             con.commit();
             con.setAutoCommit(true);
-            System.out.println("User с именем " + name + " был добавлен в базу данных.");
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Fail to save user to users table!");
         }
     }
 
     public void removeUserById(long id) {
 
-        String removeUser = "DELETE FROM USERS WHERE id= " + id;
+        String removeUser = "DELETE FROM `users` WHERE id= " + id;
 
-        try (Connection con = Util.getInstance().getConnection()) {
-            // turn off auto committing
+        try (Connection con = Util.getInstance().getConnection();
+             PreparedStatement ps = con.prepareStatement(removeUser)) {
+
             con.setAutoCommit(false);
-            // remove user by id
-            try (PreparedStatement ps = con.prepareStatement(removeUser)) {
-                ps.executeUpdate();
-            } catch (SQLException ex) {
-                // terminate transaction and roll back
-                con.rollback();
-                con.setAutoCommit(true);
-                System.out.println("Fail to delete user by!");
-            }
-            // save changes in Data Base
+            ps.executeUpdate();
             con.commit();
             con.setAutoCommit(true);
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Fail to remove user to users table!");
         }
     }
 
     public List<User> getAllUsers() {
 
-        String allUsers = "SELECT * FROM users";
+        String allUsers = "SELECT * FROM `users`";
         List<User> list = new ArrayList<>();
 
-        try (Connection con = Util.getInstance().getConnection()) {
-            // turn off auto committing
+        try (Connection con = Util.getInstance().getConnection();
+             PreparedStatement ps = con.prepareStatement(allUsers);
+             ResultSet res = ps.executeQuery()) {
+
             con.setAutoCommit(false);
-            // Acquire data from table to list
-            try (PreparedStatement ps = con.prepareStatement(allUsers);
-                 ResultSet res = ps.executeQuery()) {
-                while (res.next()) {
-                    User user = new User();
-                    user.setId(res.getLong(1));
-                    user.setName(res.getString(2));
-                    user.setLastName(res.getString(3));
-                    user.setAge(res.getByte(4));
-                    list.add(user);
-                }
-            } catch (SQLException ex) {
-                // terminate transaction and roll back
-                con.rollback();
-                con.setAutoCommit(true);
-                System.out.println("Fail to acquire user's list!");
+            while (res.next()) {
+                User user = new User();
+                user.setId(res.getLong(1));
+                user.setName(res.getString(2));
+                user.setLastName(res.getString(3));
+                user.setAge(res.getByte(4));
+                list.add(user);
             }
-            //save changes to Data Base
             con.commit();
             con.setAutoCommit(true);
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Fail to select all users from users table!");
         }
         return list;
     }
 
     public void cleanUsersTable() {
 
-        String clearTable = "DELETE FROM USERS";
+        String clearTable = "DELETE FROM `users`";
 
-        try (Connection con = Util.getInstance().getConnection()) {
-            // turn off auto committing
+        try (Connection con = Util.getInstance().getConnection();
+             PreparedStatement ps = con.prepareStatement(clearTable)) {
+
             con.setAutoCommit(false);
-            //clear table
-            try (PreparedStatement ps = con.prepareStatement(clearTable)) {
-                ps.execute();
-            } catch (SQLException ex) {
-                // terminate transaction and roll back
-                con.rollback();
-                con.setAutoCommit(true);
-                System.out.println("Fail to clear user's list!");
-            }
-            // save changes to Data Base
+            ps.executeUpdate();
             con.commit();
             con.setAutoCommit(true);
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Fail to clear users table!");
         }
     }
 }
